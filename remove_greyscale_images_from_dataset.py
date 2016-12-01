763d98c88a335ee98435e61a8b053c0f1bf37e96
Docker Image <somewhere@example.com>
2016-12-01 23:14:34 +0000

    Using CV2 for the conversions really speeds up conversion rates

--- remove_greyscale_images_from_dataset.py
+++ remove_greyscale_images_from_dataset.py
@@ -1,31 +1,30 @@
 #!/usr/bin/env python3
-import glob, subprocess, itertools, os, cv2
+import glob, subprocess, itertools, os, cv2, sys
 import numpy as np
 from joblib import Parallel, delayed
 
 
 # Winter Guerra <winterg@mit.edu> Nov. 27 2016
 
-# Minimum saturation
-min_saturation = 0.05
-dataset_location = "/root/persistant_data/datasets/places_2/train_256/"
+# Make sure that the user gave us enough arguments
+assert len(sys.argv) == 3, "Not enough arguments. EX. ./remove_greyscale_images_from_dataset.py /path/to/trainingset/ .JPEG"
+dataset_location = sys.argv[1]
+file_extension = sys.argv[2]
 num_jobs=3
 
-def get_saturation(f):
-        #command = "convert {} -colorspace HSL -channel g -separate +channel -format '%[fx:mean]' info:".format(f)
-        #op = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
-        #return float(op.stdout)
+print("Looking for files of type", file_extension, "from location", dataset_location)
 
+def get_saturation(f):
 	# Load image
 	img = cv2.imread(f)
 	# Convert BGR to HSV
 	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 	# Average the S layer
-	S_avg = np.average(hsv[:,:,1])
+	S_avg = np.average(hsv[:,:,1])/255.0 # 255 since that is the bitdepth
 	return S_avg
 
 # Get list of files
-filenames = glob.iglob(dataset_location + "**/*.jpg.greyscale", recursive=True)
+filenames = glob.iglob(dataset_location + "*/*" + file_extension)
 
 def worker_func(f):
 	# get saturation values for all files
@@ -38,4 +37,4 @@ def worker_func(f):
 
 # iterate in parallel and remove said files
 Parallel(n_jobs=num_jobs, verbose=5)(delayed(worker_func)(f) for f in filenames)
-#print(list(itertools.islice(low_saturation_results, 5)))
+#print(list(itertools.islice(filenames, 5)))
