980425fd9d3886fc4f79465f0488fbe952d30572
Isotope Server <isotope@mit.edu>
2016-12-06 05:08:57 +0000

    Parallize batch reading + run on winter's computer

--- network/run_training.py
+++ network/run_training.py
@@ -4,6 +4,19 @@ from dataloader import DataLoader
 from distribution_to_image import get_colorized_image
 import warnings
 warnings.filterwarnings('ignore')
+import time
+import threading
+
+training_batches = []
+batches_available = threading.Semaphore(0)
+DESIRED_BATCHES = 3
+
+def load_batch(dataset, BATCH_SIZE):
+  lt = time.time()
+  data_x, data_y_ = dataset.next_batch(BATCH_SIZE)
+  training_batches.append((data_x, data_y_))
+  batches_available.release()
+  print "Batch loaded in parallel ", (time.time() - lt)
 
 def run_training(BATCH_SIZE = 32, ITERATIONS = 3000):
   f = open('log.txt', 'w')
@@ -13,6 +26,7 @@ def run_training(BATCH_SIZE = 32, ITERATIONS = 3000):
 
     saver = tf.train.Saver()
     dataset = DataLoader(BATCH_SIZE)
+    dataset.next_batch(BATCH_SIZE) # hack to precompute, remove this
 
     loss = construct_graph.loss_function(y_output, y_)
     prediction = construct_graph.get_prediction(y_output)
@@ -20,15 +34,28 @@ def run_training(BATCH_SIZE = 32, ITERATIONS = 3000):
 
     sess.run(tf.initialize_all_variables())
 
-    for i in xrange(ITERATIONS):
-      data_x, data_y_ = dataset.next_batch(BATCH_SIZE)
-      _, loss_res = sess.run([train_step, loss], feed_dict={x: data_x, y_: data_y_})
+    for i in xrange(ITERATIONS): 
+      lt = time.time()
+      for b in xrange(DESIRED_BATCHES - len(training_batches)):
+        t = threading.Thread(target = load_batch, args = (dataset, BATCH_SIZE,))
+        t.start()
+      
+      batches_available.acquire()
+      data_x, data_y_ = training_batches[0]
+      del training_batches[0]
 
-      f.write(str(loss_res) + '\n'); f.flush()
+      lt2 = time.time()
+      _, loss_res = sess.run([train_step, loss], feed_dict={x: data_x, y_: data_y_})
+      lt3 = time.time()
+
+      #f.write(str(loss_res) + '\n'); f.flush()
 
       if i % 1000 == 0: 
         _colorize_and_save_test_images(sess, dataset, prediction, i/1000, x)
         saver.save( sess, 'model/model')
+
+      print "Iteration ", i, "Data loading: ", (lt2 - lt), "Backprop: ", (lt3 - lt2), "Full", (time.time() - lt), "Accuracy:", loss_res
+
 
 def _colorize_and_save_test_images(sess, dataset, prediction, iteration, x):
   test_image_batch, _ = dataset.get_test_batch()
