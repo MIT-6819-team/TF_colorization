f12750ee00facaedaa20d5ca5896db40c9e4b1be
Winter Guerra <winterg@mit.edu>
2016-12-12 01:52:25 +0000

    Using a different library makes the lab conversion much faster.

--- network/distribution_to_image.py
+++ network/distribution_to_image.py
@@ -1,13 +1,15 @@
 import numpy as np
 from PIL import Image
 from skimage import color
-from colormath.color_objects import LabColor, sRGBColor
-from colormath.color_conversions import convert_color
+#from colormath.color_objects import LabColor, sRGBColor
+#from colormath.color_conversions import convert_color
+from rio_color.colorspace import ColorSpace as cs  # enum defining available color spaces
+from rio_color.colorspace import convert_arr
 import time
 
 quantized_array = np.load('pts_in_hull.npy')
 
-def get_colorized_image( image, prediction ,use_skiimage_conversion=False):
+def get_colorized_image( image, prediction , use_skiimage_conversion=False):
     print "Get colorized image", use_skiimage_conversion
     T = 0.38
     epsilon = 1e-8
@@ -30,15 +32,8 @@ def get_colorized_image( image, prediction ,use_skiimage_conversion=False):
 def _lab_to_rgb(lab_image):
     print "Start lab to rgb. This may take a while."
     lt = time.time()
-    rgb_image = np.zeros([256, 256, 3])
-
-    for i in xrange(256**2):
-        x = i/256
-        y = i%256
-        lab = lab_image[x][y]
-        rgb = convert_color(LabColor(*lab), sRGBColor)
-        rgb_image[x][y] = np.array([rgb.clamped_rgb_r, rgb.clamped_rgb_g, rgb.clamped_rgb_b])
-
+    rgb_image = np.swapaxes(convert_arr(np.swapaxes(lab_image,0,2), src=cs.lab, dst=cs.rgb),0,2)
+    
     print "end", (time.time() - lt)
 
     return rgb_image
