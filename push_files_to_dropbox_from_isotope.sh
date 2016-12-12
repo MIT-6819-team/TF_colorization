83886eb7a9aee29027cc6c9c0ebf4ed2f138f812
Isotope Server <winterg@mit.edu>
2016-12-12 02:52:23 -0500

    Make dropbox script less dangerous

--- push_files_to_dropbox_from_isotope.sh
+++ push_files_to_dropbox_from_isotope.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-echo "Syncing network/tests to Winter's shared Dropbox every 10 minutes."
-
-#watch -n 600 -- 
-rclone sync network/tests 'Dropbox\ MIT:MIT/a_classes/6.819/final_project/' -v --transfers 20 
+ 
+rclone copy './network/tests' 'Dropbox MIT:MIT/a_classes/6.819/final_project/tests' -v --update --size-only
